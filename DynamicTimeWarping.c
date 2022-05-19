c96d618c0d8ac4e39da20fcaf53637e449897911
sgardier <75167566+sgardier@users.noreply.github.com>
2022-05-19 22:45:07 +0200

    Update DynamicTimeWarping.c

--- DynamicTimeWarping.c
+++ DynamicTimeWarping.c
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void free2dArray(void** array, unsigned int length){
-  for(unsigned int i = 0; i < length; i++){
-    free(array[i]);
-  }
-  free(array);
-}
-
 static double min3(double a, double b, double c){
   double min = a;
   if(b < min)
@@ -21,6 +14,8 @@ static double min3(double a, double b, double c){
 }
 
 double dtw(Sketch sketch1, Sketch sketch2, double maxDistance){
+  if(sketch1.points == sketch2.points)
+    return 0.0;
 
   unsigned int costMatrixWidth = sketch1.size + 1;
   unsigned int costMatrixHeight = sketch2.size + 1;
@@ -29,28 +24,36 @@ double dtw(Sketch sketch1, Sketch sketch2, double maxDistance){
   double costMatrix[costMatrixHeight][costMatrixWidth];
 
   //Initialization of the distance matrix rows
-  for(unsigned int i = 0; i < costMatrixHeight; i++){
-    if(i == 0){
-      costMatrix[0][0] = 0;
-      for(unsigned int j = 1; j < costMatrixWidth; j++){
-        costMatrix[i][j] = INFINITY;
-      }
-    }
-    else{
-      costMatrix[i][0] = INFINITY;
-    }
+  //first line
+  costMatrix[0][0] = 0;
+  for(unsigned int row1Col = 1; row1Col < costMatrixWidth; row1Col++){
+    costMatrix[0][row1Col] = +INFINITY;
+  }
+  //other lines
+  for(unsigned int row = 1; row < costMatrixHeight; row++){
+    costMatrix[row][0] = +INFINITY;
   }
 
   //Filling of the distance matrix
-  for(unsigned int i = 1; i < costMatrixWidth; i++){
-    //Process the distances from all sketch2 points to point i in sketch1 
+  for(unsigned int i = 0; i < sketch1.size; i++){
+
+    //Process the distances from all sketch2 points to point i in sketch1
+    for(unsigned int j = 0; j < sketch2.size; j++){
+      
+      double pointsDistance = ((double)(abs(sketch1.points[i].x - sketch2.points[j].x)) + (abs(sketch1.points[i].y - sketch2.points[j].y))) / 2;
+      costMatrix[j+1][i+1] = pointsDistance + min3(costMatrix[j][i], costMatrix[j][i+1], costMatrix[j+1][i]);
+    }
+    
+    unsigned int smallerValueInCol = 0;
     for(unsigned int j = 1; j < costMatrixHeight; j++){
-      double pointsDistance = ((double)(abs(sketch1.points[i - 1].x - sketch2.points[j - 1].x)) + (abs(sketch1.points[i - 1].y - sketch2.points[j - 1].y))) / 2;
-      costMatrix[j][i] = pointsDistance + min3(costMatrix[j-1][i-1], costMatrix[j-1][i], costMatrix[j][i-1]);
-      if(costMatrix[j][i] > maxDistance){
-        return INFINITY;
+      if(costMatrix[j][i+1] <= maxDistance){
+        smallerValueInCol = 1;
+        break;
       }
     }
+    if(!smallerValueInCol)
+        return +INFINITY;
   }
+
   return costMatrix[costMatrixHeight-1][costMatrixWidth-1];
 }
